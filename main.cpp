a045032419563846e87d1666cf2213f337a1dd36
Bahar ÇİFTÇİ <baharciftci94@hotmail.com>
2017-03-03 12:44:40 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,22 +1,20 @@
+#include <cstdlib>
 #include <iostream>
-
 
 using namespace std;
 
-
-int power(int a, int b){
-    int toplam = 1;
-    for(int i=0; i<b; i++){
-        toplam *= a;
+int power(int a,int b){
+    int toplam=1;
+    for(int i=0;i<b;i++){
+            toplam *=a;
     }
     return toplam;
 }
-
 int main()
 {
     int sonuc = power(2,4);
-    cout << sonuc << endl;
-    return 0;
+    cout<<sonuc<<endl;
+    system("PAUSE");
+    return EXIT_SUCCESS;
+    
 }
-
-
